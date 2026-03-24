419bc0dd98bff32dfd93ee41e82c3a8771118956
xxxtools <code@colamail.netlib.re>
2026-03-24 23:05:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ import subprocess
 from aiohttp import web
 
 # 环境变量
-UUID = os.environ.get('UUID', '7bd180e8-1142-4387-93f5-03e8d750a896')   # 节点UUID
-NEZHA_SERVER = os.environ.get('NEZHA_SERVER', '')    # 哪吒v0填写格式: nezha.xxx.com  哪吒v1填写格式: nezha.xxx.com:8008
+UUID = os.environ.get('UUID', '52ec8be9-7e68-4e95-88d4-86a2c0b13206')   # 节点UUID
+NEZHA_SERVER = os.environ.get('NEZHA_SERVER', 'tta.wahaaz.xx.kg:80')    # 哪吒v0填写格式: nezha.xxx.com  哪吒v1填写格式: nezha.xxx.com:8008
 NEZHA_PORT = os.environ.get('NEZHA_PORT', '')        # 哪吒v1请留空，哪吒v0 agent端口
-NEZHA_KEY = os.environ.get('NEZHA_KEY', '')          # 哪吒v0或v1密钥，哪吒面板后台命令里获取
+NEZHA_KEY = os.environ.get('NEZHA_KEY', 'OZMtCS6G39UpEgRvzRNXjS7iDNBRmTsI')          # 哪吒v0或v1密钥，哪吒面板后台命令里获取
 DOMAIN = os.environ.get('DOMAIN', '')                # 项目分配的域名或反代后的域名,不包含https://前缀,例如: domain.xxx.com
 SUB_PATH = os.environ.get('SUB_PATH', 'sub')         # 节点订阅token
-NAME = os.environ.get('NAME', '')                    # 节点名称
+NAME = os.environ.get('NAME', 'renus')                    # 节点名称
 WSPATH = os.environ.get('WSPATH', UUID[:8])          # 节点路径
 PORT = int(os.environ.get('SERVER_PORT') or os.environ.get('PORT') or 3000)  # http和ws端口，默认自动优先获取容器分配的端口
 AUTO_ACCESS = os.environ.get('AUTO_ACCESS', '').lower() == 'true' # 自动访问保活,默认关闭,true开启,false关闭,需同时填写DOMAIN变量
